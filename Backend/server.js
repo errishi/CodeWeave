9347c96a06c18195068014e2599c6db3134259c3
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-04-02 22:20:32 +0530

    server setup

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { YSocketIO } from 'y-socket.io/dist/server';
+
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server({
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize();
+
+
+app.get("/", (req,res)=> {
+    res.status(200).json({
+        success: true,
+        message: "Hello World"
+    });
+});
+
+app.get("/health", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: "ok"
+    });
+});
+
+
+httpServer.listen(3000, ()=>{
+    console.log("Server is listen on port 3000");
+});
